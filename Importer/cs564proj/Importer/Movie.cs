ff91d0084f0058918a54ea30b2ded78712db7f51
Garrett Low <usernamegl@live.com>
2021-06-27 14:34:46 -0500

    import start: read movies.csv

---
+++ Importer/cs564proj/Importer/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileHelpers;
+
+namespace Importer
+{
+    [DelimitedRecord(","), IgnoreFirst(1)]
+    class Movie
+    {
+        [FieldOrder(1), FieldCaption("movieId")]
+        public int Id { get; set; }
+        [FieldOrder(2), FieldCaption("title"), FieldQuoted('"', QuoteMode.OptionalForBoth)]
+        public string Title { get; set; }
+    [FieldOrder(3), FieldCaption("genres"), FieldDelimiter("|")]
+        public string[] GenreArray { get; set; }
+    }
+}
---
+++ Importer/cs564proj/Importer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FileHelpers;
+
+namespace Importer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new FileHelperEngine<Movie>();
+            var movies = engine.ReadFile("W:\\source\\testFiles\\cs564\\ml-latest-small\\movies.csv");
+            foreach (var movie in movies)
+            {
+                Console.Write(movie.Id + ", " + movie.Title + ", ");
+                foreach (string genre in movie.GenreArray)
+                {
+                    Console.Write(genre + ", ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
