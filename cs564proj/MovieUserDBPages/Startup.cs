6883a7f45e4112a7da74e57f92f5d664aea38efe
kboening <43833882+kboening@users.noreply.github.com>
2021-07-19 20:34:50 -0500

    query strings for user and such

--- cs564proj/MovieUserDBPages/Startup.cs
+++ cs564proj/MovieUserDBPages/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+// using Importer; 
 
 namespace LetterBoxDClone
 {
@@ -24,6 +26,7 @@ namespace LetterBoxDClone
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<Importer.MovieContext>(options => options.UseSqlite(Configuration.GetConnectionString("Importer.MovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
