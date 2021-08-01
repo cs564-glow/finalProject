a7f188257dbf8a303260519c97fe1bb17e2bbd2b
Garrett Low <usernamegl@live.com>
2021-08-01 16:53:43 -0500

    user pagination

--- cs564proj/LetterBoxDClone/Pages/Users/Index.cshtml.cs
+++ cs564proj/LetterBoxDClone/Pages/Users/Index.cshtml.cs
@@ -6,23 +6,63 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DataLibrary;
+using Microsoft.Extensions.Configuration;
 
 namespace LetterBoxDClone.Pages.Users
 {
     public class IndexModel : PageModel
     {
-        private readonly DataLibrary.MovieContext _context;
+        private readonly MovieContext _context;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(DataLibrary.MovieContext context)
+        public IndexModel(MovieContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
-        public IList<User> User { get;set; }
+        public string UsernameSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public PaginatedList<User> UserList { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            User = await _context.User.ToListAsync();
+            CurrentSort = sortOrder;
+            UsernameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            CurrentFilter = searchString;
+
+            IQueryable<User> userIq = from c in _context.User
+                                              select c;
+
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                // case sensitivity: https://docs.microsoft.com/en-us/aspnet/core/data/ef-rp/sort-filter-page?view=aspnetcore-5.0#iqueryable-vs-ienumerable
+                userIq = userIq.Where(c => c.Username.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    userIq = userIq.OrderByDescending(c => c.Username);
+                    break;
+                default:
+                    userIq = userIq.OrderBy(c => c.Username);
+                    break;
+            }
+
+            var pageSize = _configuration.GetValue("PageSize", 10);
+
+            UserList = await PaginatedList<User>.CreateAsync(userIq.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
